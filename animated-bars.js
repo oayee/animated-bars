c08a8357887423269b311fb47cba8b38a0668f9a
oayee <selass.selass.selass@gmail.com>
2019-01-29 18:00:02 +0300

    fixed sign; updated height calc function, removed inline background flip changed to be both hor\vert

--- animated-bars.js
+++ animated-bars.js
@@ -2,44 +2,45 @@
 
 let resizeTimeout;
 
-function drawDiv(elToAppend, color = 'blue', sign, barX, barWidth = 5) {
-    const height = 50 + sign * Math.random() * 40;
+function drawDiv(elToAppend, sign, barX, barWidth = 5) {
+    const height = ~sign ? 100 - Math.random() * 45 : 5 + Math.random() * 35;
     const rect = document.createElement('div');
-    rect.style = `left: ${barX}px; width: ${barWidth}px; height: ${height}px; background: ${color}`;
+    rect.style = `left: ${barX}px; width: ${barWidth}px; height: ${height}px;`;
     rect.setAttribute('class', 'divbar noheight');
     elToAppend.appendChild(rect);
-    sign *= -1;
 }
 
-function drawDivs(id, color) {
+function drawDivs(id, fliped = false) {
     const container = document.getElementById(id);
     if (!container) return;
     if (container.firstChild) container.removeChild(container.firstChild);
     const div = document.createElement('div');
+    const classStr = 'innerdiv' + (fliped ? ' innerdiv-fliped' : '');
     div.setAttribute('id', 'div');
-    div.setAttribute('class', 'innerdiv');
+    div.setAttribute('class', classStr);
     container.appendChild(div);
     const width = container.clientWidth;
     const numOfRects = Math.floor(width / 10);
     let sign = 1;
     for (let i = 0; i < numOfRects; i++) {
-        drawDiv(div, color, sign, i * 10);
+        drawDiv(div, sign, i * 10);
+        sign *= -1;
     }
 
     // draw last truncated bar
     const lastBarWidth = width % 10 > 5 ? 5 : width % 10;
-    drawDiv(div, color, sign, numOfRects * 10, lastBarWidth);
+    drawDiv(div, sign, numOfRects * 10, lastBarWidth);
 }
 
 function onResize() {
     if (!resizeTimeout) {
         resizeTimeout = setTimeout(function () {
             resizeTimeout = null;
-            drawDivs('container-upper', 'blueviolet');
-            drawDivs('container-lower', 'aqua');
+            drawDivs('container-upper');
+            drawDivs('container-lower', true);
             setTimeout(function () {
-                resizeCallback('container-upper', 'blueviolet');
-                resizeCallback('container-lower', 'aqua');
+                resizeCallback('container-upper');
+                resizeCallback('container-lower');
             }, 100);
         }, 300);
     }
@@ -53,12 +54,12 @@ function resizeCallback(id) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    drawDivs('container-upper', 'blueviolet');
-    drawDivs('container-lower', 'aqua');
+    drawDivs('container-upper');
+    drawDivs('container-lower', true);
 
     setTimeout(function () {
-        resizeCallback('container-upper', 'blueviolet');
-        resizeCallback('container-lower', 'aqua');
+        resizeCallback('container-upper');
+        resizeCallback('container-lower');
     }, 500);
 });
 window.addEventListener("resize", onResize);
